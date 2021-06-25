d53d443ce9bdb597d8f5c725d2aa2148bd65e874
Sara Peña <s.cristal.p@gmail.com>
2021-06-25 13:29:24 -0400

    imported components to app.module.ts

--- NouSystems/src/app/app.module.ts
+++ NouSystems/src/app/app.module.ts
@@ -3,11 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './nou-systems/button/button.component';
+
+import { ContainerComponent } from './nou-systems/container/container.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ContainerComponent,
+    ButtonComponent
     
   ],
   imports: [
